9fb8fcb3ec0dc1aa90904bdae2c078cf8a01d745
Clement Balay <cle.balay@gmail.com>
2012-08-13 14:35:25 +0100

    PLF-3443 - [Setup Wizard] Create WS writeProperties

--- exo-setup/config/src/main/java/org/exoplatform/setup/rest/SetupWizardRestService.java
+++ exo-setup/config/src/main/java/org/exoplatform/setup/rest/SetupWizardRestService.java
@@ -1,18 +1,24 @@
 package org.exoplatform.setup.rest;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.CacheControl;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
+import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.log4j.Logger;
 import org.exoplatform.services.rest.resource.ResourceContainer;
@@ -43,10 +49,53 @@ public class SetupWizardRestService implements ResourceContainer {
     return Response.ok(Collections.emptyList(), MediaType.APPLICATION_JSON).build();
   }
   
-  @GET
+  @POST
   @Path("/wp")
   @Produces(MediaType.APPLICATION_JSON)
-  public Response writeProperties() {
+  public Response writeProperties(@PathParam("toto") String toto, @Context UriInfo uriInfo) {
+    
+    if(logger.isDebugEnabled()) {
+      logger.debug("writeProperties is called");
+    }
+    
+    MultivaluedMap<String, String> queryParams = uriInfo.getQueryParameters();
+
+    if(queryParams != null && queryParams.size() > 0) {
+      try {
+        PropertiesConfiguration exoConf = getExoConfiguration();
+        Properties systemProperties = System.getProperties();
+  
+        // Fetch all properties stores by user
+        for(Map.Entry<String, List<String>> entry : queryParams.entrySet()) {
+          String ppName = entry.getKey();
+          String ppValue = entry.getValue().get(0);
+          
+         if(ppName != null && ppValue != null) {
+            if(exoConf.containsKey(ppName)) {
+              // If property exists we update it
+              exoConf.setProperty(ppName, ppValue);
+            }
+            else {
+              // Else we add to file
+              exoConf.addProperty(ppName, ppValue);
+            }
+            // Update System Properties
+            systemProperties.setProperty(ppName, ppValue);
+          }
+        }
+        exoConf.save();
+      }
+      catch (ConfigurationException e) {
+        logger.error("Cannot save Properties configuration", e);
+      }
+      catch(WizardPropertiesException e) {
+        logger.error("Cannot get exo configuration", e);
+      }
+      catch(Exception e) {
+        logger.error("Problem with WS writeProperties", e);
+      }
+    }
+    
     return Response.ok(Collections.emptyList(), MediaType.APPLICATION_JSON).build();
   }
   
